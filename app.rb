a9ca8a2a322acbd7223fb90b91fc34745aa1947b
Reno Sorensen <renojames19@gmail.com>
2019-05-08 05:14:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -36,7 +36,7 @@ class App < Sinatra::Base
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
 
-    binding.pry
+    #binding.pry
 
     if @operation == "add"
       "#{@number1 + @number2}"
